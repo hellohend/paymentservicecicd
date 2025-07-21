87da65055582a41a8cbe2d8ff5e7e136327ddbd7
cisnux <fajrarisqulla@gmail.com>
2025-07-21 09:17:55 +0700

    feat: init

--- migrations/V2__create_authentications_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE authentications
-(
-    token      VARCHAR(512) PRIMARY KEY,
-    user_id    SERIAL NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    created_at TIMESTAMP   NOT NULL DEFAULT now()
-);
-
-CREATE INDEX idx_authentications_user_id ON authentications(user_id);
-CREATE INDEX idx_authentications_created_at ON authentications(created_at);
